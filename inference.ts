2310db0fd9c63b3dd9482d4a2fa5773a62fd39a1
Biniyam Mulugeta <biniyammulugetabm@gmail.com>
2024-05-22 19:18:25 +0300

    adding inference concept

---
+++ inference.ts
@@ -0,0 +1,10 @@
+// let food = "🍕Pizza";
+// food = "🍔Hamburger";
+// console.log(food);
+// console.log(typeof food);
+
+let isMale = true;
+isMale = "hello";
+
+console.log(isMale)
+console.log(typeof isMale)
