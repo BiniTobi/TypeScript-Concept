a334e16b5b01f2223857ca78cac20df8a2fd57fe
Biniyam Mulugeta <biniyammulugetabm@gmail.com>
2024-05-22 07:48:34 +0300

    adding first Ts concepts files

---
+++ anotaion.ts
@@ -0,0 +1,13 @@
+// string
+
+// let firstName : string ="BinuterX";
+// console.log(firstName);
+
+// number
+// let myAge : number = 11;
+// console.log(myAge);
+
+// boolean
+let isStudent : boolean = false;
+
+console.log(isStudent);
---
+++ index.ts
@@ -0,0 +1,10 @@
+type Animals ={
+    name: string;
+}
+
+const animals : Animals={
+    name:"dog",
+}
+
+console.log(animals.name)
+
