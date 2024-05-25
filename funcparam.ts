15ba5fdbd15fa7f5208762b44659e796adfdc4eb
Biniyam Mulugeta <biniyammulugetabm@gmail.com>
2024-05-25 07:53:31 +0300

    funciton params annotation

---
+++ funcparam.ts
@@ -0,0 +1,20 @@
+// function addNumbers(number1:number,number2:number){
+//     const reuslt = number1 + number2;
+//     console.log(reuslt);
+// }
+
+// addNumbers(2,3);
+// you can work with arrow function also
+// const substractNum = (number1:number,number2:number)=>{
+//         const reuslt = number1 - number2;
+
+//         console.log(reuslt);
+// }
+
+// substractNum(8,4);
+
+function sayHi(name:boolean){
+    console.log("hello",name);
+}
+
+sayHi(true);
