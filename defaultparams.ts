32b77aa51b19f9e6f77881a5a2b738e4c3580fa5
Biniyam Mulugeta <biniyammulugetabm@gmail.com>
2024-05-26 10:14:21 +0300

    default params in ts

---
+++ defaultparams.ts
@@ -0,0 +1,13 @@
+// function myFavoriteFood(food:string = "🍕pizza"){
+//     return `my favorite food is ${food}`;
+
+// }
+
+// const myfav = myFavoriteFood();
+// console.log(myfav);
+
+const isSunny = (issunny:boolean = true)=>{
+    return `Today is sunny?☀️ ${issunny}`
+}
+const sunny = isSunny();
+console.log(sunny);
