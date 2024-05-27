15972634de31d53d13451e9c213d07109a1c0d3b
Biniyam Mulugeta <biniyammulugetabm@gmail.com>
2024-05-27 10:41:17 +0300

    adding the function return annotation and void function in ts

---
+++ funretann.ts
@@ -0,0 +1,24 @@
+// function addition(number1:number,number2:number):number{
+//         return number1 + number2;
+// }
+
+// const result = addition(3,2);
+// console.log(result);
+
+
+// arrow function with function return type
+
+// const substraction = (number1:number,number2:number):number=> {
+//     return number1 - number2;
+// }
+
+// const result = substraction(8,4);
+// console.log(result);
+
+// string
+
+const myFavoriteCar = (car:string):string=>{
+    return `My favorite car is ${car}`;
+}
+const mycar = myFavoriteCar("Ford");
+console.log(mycar);
---
+++ voidfunc.ts
@@ -0,0 +1,18 @@
+// function bayMax(name:string):void{
+//     console.log(`🤖Hello im baymax your personal health care ${name} how are you?`)
+// }
+
+// bayMax("binuterx");
+
+
+// const myFavorteColor = (color:string):void=>{
+//     console.log(`My favorite color is ${color}`);
+// }
+
+// myFavorteColor("blue");
+
+const addition = (number1:number,number2:number):void=>{
+    console.log(`The sum of ${number1} and ${number2} is ${number1+number2}`);
+}
+
+addition(4,4);
