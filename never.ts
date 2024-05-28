ec4146c49450bf1f167da28449e5b2ef4dd029ab
Biniyam Mulugeta <biniyammulugetabm@gmail.com>
2024-05-28 09:32:07 +0300

    never keyword

---
+++ never.ts
@@ -0,0 +1,20 @@
+// // funtion always throws an error 
+// function myError(error:string):never{
+//     throw new Error("This is an error"+error);
+// }
+
+// 
+
+// const myInfinite = ():never=>{
+//     while(true){
+
+//     }
+// }
+
+// let x : number;
+
+// function calculator():never{
+//     while(true){}
+// }
+
+// x = calculator();
