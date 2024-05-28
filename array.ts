0fb72d3095d7c716bf5f393d8ad2a4366fc0724a
Biniyam Mulugeta <biniyammulugetabm@gmail.com>
2024-05-28 17:48:44 +0300

    array type in ts

---
+++ array.ts
@@ -0,0 +1,19 @@
+//1 WAY
+//  String Array Types
+
+
+// let myFavoriteFoods: string[] = ["🍕pizza", "🍔hamburger", "🍣sushi"];
+
+
+// console.log(myFavoriteFoods);
+
+// Number Array Types
+
+// let myLuckyNumber:number[] = [7,1,8];
+// console.log(myLuckyNumber);
+
+// 2 WAY 
+
+// const myFavoriteMovies:Array<string> = ["Spiderman","Avengers","Batman"];
+// const myFavoriteNumbers:Array<number> = [1,3,5,4,];
+// console.log(myFavoriteMovies);
