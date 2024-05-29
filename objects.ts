e8ff650e3f8aaff49d1bba5a6e130f11d385dbf3
Biniyam Mulugeta <biniyammulugetabm@gmail.com>
2024-05-29 18:28:35 +0300

    hello

---
+++ objects.ts
@@ -0,0 +1,10 @@
+
+
+const book:{title:string,autor:string,year:number,avalable:boolean} = {
+    title: "Harry Potter and the Philosopher's Stone",
+    autor:"J.K. Rowling",
+    year: 1997,
+    avalable: true
+}
+
+console.log(`${book.title} by ${book.autor} (${book.year}) available: ${book.avalable}`)
