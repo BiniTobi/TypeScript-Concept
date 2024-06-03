66022866fbf9840d2859c30336f3c4bfddbe4b96
Biniyam Mulugeta <biniyammulugetabm@gmail.com>
2024-06-03 11:34:56 +0300

    tuples and literal

---
+++ litral.ts
@@ -0,0 +1,17 @@
+
+
+// let footBallPlayer: "NeymarJr" | "Messi" | "Ronaldo";
+
+// footBallPlayer = "NeymarJr";
+
+// console.log(footBallPlayer);
+
+// let myNumbersList : 1 |3 |5;
+
+// myNumbersList = 3;
+// console.log(myNumbersList)
+
+// let typeScriptIsGood : true | false;
+
+// typeScriptIsGood =false;
+// console.log(typeScriptIsGood);
---
+++ tuple.ts
@@ -0,0 +1,9 @@
+let myFavoriteFood: [string,string,string,boolean,number] =["🍕pizza","🍔hamburger","🍣sushi",true,12];
+
+let [first,second,third,forth,fifth] = myFavoriteFood;
+
+console.log(first);
+console.log(second);
+console.log(third);
+console.log(forth);
+console.log(fifth)
