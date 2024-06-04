27b9a4d68ef19eebfb9dcb2afa29afdcfbce2077
Biniyam Mulugeta <biniyammulugetabm@gmail.com>
2024-06-04 07:35:03 +0300

    adding enums

---
+++ enums.ts
@@ -0,0 +1,14 @@
+enum myGymWorkoutPlan{
+    Day1="Push.Barbell bench press",
+    Day2="Pull. Barbell deadshifts",
+    Day3="Legs.Barbell Squats",
+    Day4="push push up",
+    Day5=" pull pull up",
+    Day6="Legs. Goblet Squat",
+    Day7= "Rest Day"
+}
+
+
+const myWorkoutPlanDay6 = myGymWorkoutPlan.Day6;
+
+console.log(myWorkoutPlanDay6);
